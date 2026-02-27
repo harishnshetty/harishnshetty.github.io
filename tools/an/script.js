61533d4b61b8e0f2ee2044938aa11b7db1da22fd
harishnshetty <harishn662@gmail.com>
2026-02-27 10:11:01 +0530

    fix(zip): fix basicPkgs reference error causing zip downloader to crash implicitly

--- tools/an/script.js
+++ tools/an/script.js
@@ -303,6 +303,8 @@ function pkgModule(os) {
 }
 
 // Special package names per OS
+
+
 const pkgNames = {
   mysql: { ubuntu: 'mysql-server', debian: 'mysql-server' },
   docker: { ubuntu: 'docker.io', debian: 'docker.io', redhat: 'docker-ce', centos: 'docker-ce', amazon: 'docker', alpine: 'docker' },
